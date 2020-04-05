bd94d6bcf84430bc5f90f737f8c5b22707cd0836
akshaya <akshayask29@gmail.com>
2020-04-05 19:24:19 +0530

    Initial commit

---
+++ test3/quiz.js
@@ -0,0 +1,89 @@
+var ul=document.getElementById('ul');
+   var btn=document.getElementById('button');
+   var scoreCard=document.getElementById('scoreCard');
+   var quizBox=document.getElementById('questionBox');
+  var op1=document.getElementById('op1');
+  var op2=document.getElementById('op2');
+  var op3=document.getElementById('op3');
+  var op4=document.getElementById('op4');
+
+
+      var app={
+                questions:[
+                          {q:'Shakespeare:', options:['Romeo ','Hamlet ',' Novel','Play'],answer:3},
+
+                          {q:'VIBGYOR:',options:['Red','Indigo','Olive','Yellow'],answer:3},
+
+                          {q:'Mathematics',options:['Arithmetic','Trigonometry','Quantum Machines','Geometry'],answer:3}
+                          ],
+                index:0,
+                load:function(){
+                	   if(this.index<=this.questions.length-1){
+                        quizBox.innerHTML=this.index+1+". "+this.questions[this.index].q;      
+                        op1.innerHTML=this.questions[this.index].options[0];
+                        op2.innerHTML=this.questions[this.index].options[1];
+                        op3.innerHTML=this.questions[this.index].options[2];
+                        op4.innerHTML=this.questions[this.index].options[3];
+                           this.scoreCard();
+                        }
+                        else{
+
+                        quizBox.innerHTML="Quiz Over......!!!"      
+                        op1.style.display="none";
+                        op2.style.display="none";
+                        op3.style.display="none";
+                        op4.style.display="none";
+                        btn.style.display="none";
+                        }
+                },
+                 next:function(){
+                    this.index++;
+                    this.load();
+                 },
+                check:function(ele){
+                   
+                         var id=ele.id.split('');
+                         
+                         if(id[id.length-1]==this.questions[this.index].answer){
+                         	this.score++;
+                         	ele.className="correct";
+                         	ele.innerHTML="Correct";
+                         	this.scoreCard();
+                         }
+                         else{
+                         	ele.className="wrong";
+                         	ele.innerHTML="Wrong";
+                         }
+                },
+                notClickAble:function(){
+                       for(let i=0;i<ul.children.length;i++){
+                       	    ul.children[i].style.pointerEvents="none";
+                       }
+                },
+
+                clickAble:function(){
+                       for(let i=0;i<ul.children.length;i++){
+                       	    ul.children[i].style.pointerEvents="auto";
+                       	    ul.children[i].className=''
+
+                       }
+                },
+                score:0,
+                scoreCard:function(){
+                	scoreCard.innerHTML=this.questions.length+"/"+this.score;
+                }
+ 
+           }
+
+
+           window.onload=app.load();
+
+           function button(ele){
+           	     app.check(ele);
+           	     app.notClickAble();
+           }
+
+         function  next(){
+              app.next();
+              app.clickAble();
+         } 
---
+++ test4/quiz.js
@@ -0,0 +1,89 @@
+var ul=document.getElementById('ul');
+   var btn=document.getElementById('button');
+   var scoreCard=document.getElementById('scoreCard');
+   var quizBox=document.getElementById('questionBox');
+  var op1=document.getElementById('op1');
+  var op2=document.getElementById('op2');
+  var op3=document.getElementById('op3');
+  var op4=document.getElementById('op4');
+
+
+      var app={
+                questions:[
+                          {q:'Select Correct Word', options:['Iminent ','Imminent ','Iminnent ','Imineent '],answer:2},
+
+                          {q:'Select Correct Word',options:['Callibration ','Calibration ','Celibration ','Calibrration '],answer:2},
+
+                          {q:'Select Correct Word',options:['Asassination ','Assasination', 'Assassination ','Assessination '],answer:3}
+                          ],
+                index:0,
+                load:function(){
+                	   if(this.index<=this.questions.length-1){
+                        quizBox.innerHTML=this.index+1+". "+this.questions[this.index].q;      
+                        op1.innerHTML=this.questions[this.index].options[0];
+                        op2.innerHTML=this.questions[this.index].options[1];
+                        op3.innerHTML=this.questions[this.index].options[2];
+                        op4.innerHTML=this.questions[this.index].options[3];
+                           this.scoreCard();
+                        }
+                        else{
+
+                        quizBox.innerHTML="Quiz Over......!!!"      
+                        op1.style.display="none";
+                        op2.style.display="none";
+                        op3.style.display="none";
+                        op4.style.display="none";
+                        btn.style.display="none";
+                        }
+                },
+                 next:function(){
+                    this.index++;
+                    this.load();
+                 },
+                check:function(ele){
+                   
+                         var id=ele.id.split('');
+                         
+                         if(id[id.length-1]==this.questions[this.index].answer){
+                         	this.score++;
+                         	ele.className="correct";
+                         	ele.innerHTML="Correct";
+                         	this.scoreCard();
+                         }
+                         else{
+                         	ele.className="wrong";
+                         	ele.innerHTML="Wrong";
+                         }
+                },
+                notClickAble:function(){
+                       for(let i=0;i<ul.children.length;i++){
+                       	    ul.children[i].style.pointerEvents="none";
+                       }
+                },
+
+                clickAble:function(){
+                       for(let i=0;i<ul.children.length;i++){
+                       	    ul.children[i].style.pointerEvents="auto";
+                       	    ul.children[i].className=''
+
+                       }
+                },
+                score:0,
+                scoreCard:function(){
+                	scoreCard.innerHTML=this.questions.length+"/"+this.score;
+                }
+ 
+           }
+
+
+           window.onload=app.load();
+
+           function button(ele){
+           	     app.check(ele);
+           	     app.notClickAble();
+           }
+
+         function  next(){
+              app.next();
+              app.clickAble();
+         } 
